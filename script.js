804bf0a20a738570b1fa19bb77de6dbdb2e83d8c
Nikhil <123582746+NikhilKalloli@users.noreply.github.com>
2024-11-20 14:10:13 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('demoForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    alert('Thank you for submitting the form!');
+});
